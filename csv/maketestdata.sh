108a80c589306e2d5043336e55cbe524e55c0d4a
David Rolfe <drolfe@voltdb.com>
2022-05-06 08:41:34 +0100

    data fixes

---
+++ csv/maketestdata.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm Nov09JnyExport.csv 2> /dev/null
+
+for i in Nov09JnyExport.csv.a?
+do
+ 	cat $i >> Nov09JnyExport.csv
+done
+
+sh createTestDataMidnightFix.sh > /dev/null
+sh assembleCsvFileByDay.sh	
+
+split -l 400000 subwaytestfullweek.csv subwaytestfullweek.csv.
